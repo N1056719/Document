b69d7c07b8f1d495f1b91f98a06fe13db57ce006
ChiangHanLung <johnnychiang@msn.com>
2017-01-01 16:55:19 +0800

    新增檔案

--- WorkCode/Terminal/Scripts/PageLoad.js
+++ WorkCode/Terminal/Scripts/PageLoad.js
@@ -1,24 +1,28 @@
 // 頁面第一次載入時的處理程序
 function formLoad() {
+	// 處理標題圖片輪播
+	$('#headImage').attr('src', '../Images/enjoy120.jpg');
 	// 取出每一個 Input 物件
 	$('input[type=image]').each(function () {
-		// 取出物件的 ID，組成 JQuery 的物件
-		var idName = '#' + this.id;
-		// 加入 OnClick 的事件處理程序
-		$(idName).on('click', summit);
-		// 加入 滑鼠移入與移出 的事件處理程序
-		$(idName).hover(mouseEnter, mouseExit);
 		// 取得按鈕 ID 最後一個數字字元
 		var idNum = this.id.substring(this.id.length - 8);
 		console.log(idNum);
 		// 經過計算後決定要開啟的圖片
 		var imageFileName = '../Images/' + idNum + '11.jpg';
+		console.log(imageFileName);
+		// 取出物件的 ID，組成 JQuery 的物件
+		var idName = '#' + this.id;
 		$(idName).attr('src', imageFileName);
-		$(idName).attr('width', '1260px');
-		$(idName).attr('height', '60px');
+		// 加入事件處理程序
+		// 加入 OnClick 的事件處理程序
+		$(idName).click(summit);
+		// 加入 滑鼠移入與移出 的事件處理程序
+		$(idName).hover(mouseEnter, mouseExit);
 	})
 	// 版權宣告
 	$('#copyRight').attr('src', '../Images/copyright.jpg');
+	$('#copyRight').attr('width', '1260px');
+	$('#copyRight').attr('height', '60px');	
 }
 // 滑鼠移到圖片上時的的處理程序
 function mouseEnter() {
